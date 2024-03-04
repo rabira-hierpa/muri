186192667f1edbd82dbb0b4aa54f608d6053dc1b
rabra <rabira.hierpa@gmail.com>
2024-03-04 14:14:38 +0300

    Add populationMap and populationPrediction types

---
+++ src/utils/helpers/index.ts
@@ -0,0 +1,19 @@
+export const populationMap = {
+  AF: {
+    population: 1_300_000,
+    growthRate: 1.2,
+    years: 10,
+  },
+  AX: {
+    population: 2_500_00,
+    growthRate: 1.7,
+    years: 5,
+  },
+  AL: {
+    population: 2_800_000,
+    growthRate: 2.2,
+    years: 15,
+  },
+};
+
+export type populationPrediction = number;
