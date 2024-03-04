e6a815430fcdc344e5fd4a050c170252645f2c94
rabra <rabira.hierpa@gmail.com>
2024-03-04 14:14:31 +0300

    Add population prediction worker

---
+++ src/utils/workers/populationPrediction.worker.ts
@@ -0,0 +1,26 @@
+import { populationPrediction } from "../helpers";
+/* eslint-disable no-restricted-globals */
+self.name = "populationPrediction.worker";
+
+self.onmessage = (event) => {
+  const { population, growthRate, years } = event.data;
+  console.log({ population, growthRate, years });
+  let populationPrediction: populationPrediction[] = [];
+  let tempPop = population;
+  let i = 1;
+
+  // set the loop to delay by 5 seconds using setTimeout
+  const process = setInterval(() => {
+    if (i <= years) {
+      const result = tempPop * (1 + growthRate / 100);
+      tempPop = result;
+      populationPrediction.push(Math.floor(result));
+      console.log(`Calculated result for year ${i} is ${populationPrediction}`);
+      i += 1;
+    } else {
+      // clear the interval
+      clearInterval(process);
+      self.postMessage(populationPrediction);
+    }
+  }, 500);
+};
